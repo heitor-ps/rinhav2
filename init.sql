79aacf7e772d4ab013f3b11e49ffbe5e33444152
Jordan Rocha <70rd23@gmail.com>
2024-02-13 23:48:31 -0300

    change postgres configs to external file

--- init.sql
+++ init.sql
@@ -1,19 +1,3 @@
-DO
-$$
-
-BEGIN
-	SET statement_timeout = 0;
-	SET lock_timeout = 0;
-	SET idle_in_transaction_session_timeout = 0;
-	SET client_encoding = 'UTF8';
-	SET standard_conforming_strings = on;
-	SET check_function_bodies = false;
-	SET xmloption = content;
-	SET client_min_messages = warning;
-	SET row_security = off;
-	SET default_tablespace = '';
-	SET default_table_access_method = heap;
-
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY,
 	balance INTEGER,
@@ -41,6 +25,3 @@ VALUES
 (3, 1000000, 0),
 (4, 10000000, 0),
 (5, 500000, 0);
-
-END
-$$;
